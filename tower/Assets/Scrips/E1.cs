16d351f386b6fc3133a52b6ac4c2a73256e9e8c3
eafun_huang <13645990236@163.com>
2020-07-02 08:27:07 +0800

    地图 + 塔

---
+++ tower/Assets/Scrips/E1.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class E1 : MonoBehaviour
+{
+    public float speed = 10;
+    private Transform[] positions;
+    private int index =0;
+     void Start()
+    {
+        positions = WayPoint.positions;
+    }
+     void Update()
+    {
+
+
+        Move();
+    }
+    void Move()
+    {
+        if (index > positions.Length - 1) return;
+        transform.Translate((positions[index].position - transform.position).normalized * Time.deltaTime * speed);
+        if (Vector3.Distance(positions[index].position, transform.position) < 0.2f)
+        {
+            index++;
+        }
+        //if (index > positions.Length - 1)
+        //{
+        //    ReachDestination();
+        //}
+        //void ReachDestination()
+        //{
+        //    GameManager.Instance.Failed();
+        //    GameObject.Destroy(this.gameObject);
+        //}
+    }
+}
---
+++ tower/Assets/Scrips/ViewControl.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewControl : MonoBehaviour
+{
+    public float speed;
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        transform.Translate(new Vector3(v, 0,-h)*Time.deltaTime*speed,Space.World);
+       
+
+    }
+}
